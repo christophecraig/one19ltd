ac03e4b7753b1f5343b0298d0013e77295044fb9
Christophe Craig <christophe.craig@viacesi.fr>
2020-07-27 19:35:23 +1200

    removed block-library stylesheet

--- wp-content/themes/one19limited/functions.php
+++ wp-content/themes/one19limited/functions.php
@@ -223,6 +223,13 @@ function one19limited_scripts()
 }
 add_action('wp_enqueue_scripts', 'one19limited_scripts');
 
+// Remove unwanted styles
+function dm_remove_wp_block_library_css()
+{
+    wp_dequeue_style('wp-block-library');
+}
+add_action('wp_enqueue_scripts', 'dm_remove_wp_block_library_css');
+
 /**
  * Implement the Custom Header feature.
  */
