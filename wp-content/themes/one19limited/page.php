2a2901fb122110a728f7fc4823239e1b80f18e49
Christophe Craig <christophe.craig@viacesi.fr>
2020-08-21 19:16:24 +1200

    wip (just committing in case something fails when changing my SSD)

--- wp-content/themes/one19limited/page.php
+++ wp-content/themes/one19limited/page.php
@@ -12,26 +12,13 @@
  * @package one19limited
  */
 
-get_header(); ?>
+use Timber\Post;
+use Timber\Timber;
 
-<div class="box">
-    <main id="primary" class="site-main">
-        <?php while (have_posts()):
-        the_post();
-
-        get_template_part('template-parts/content', 'page');
-
-        // If comments are open or we have at least one comment, load up the comment template.
-        if (comments_open() || get_comments_number()):
-            comments_template();
-        endif;
-    endwhile;
-// End of the loop.
+get_header();
 ?>
 
-    </main><!-- #main -->
-</div>
-
 <?php
-get_sidebar();
-get_footer();
+$context = Timber::context();
+$context['page'] = new Post();
+Timber::render('page.twig', $context);
