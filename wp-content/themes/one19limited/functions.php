f102cc8c4acc8b6ae14a47eb7cdce16061b15b5b
Christophe Craig <christophe.craig@viacesi.fr>
2020-08-28 00:52:59 +1200

    remove jetpack.css

--- wp-content/themes/one19limited/functions.php
+++ wp-content/themes/one19limited/functions.php
@@ -274,3 +274,5 @@ require get_template_directory() . '/inc/customizer.php';
 if (defined('JETPACK__VERSION')) {
     require get_template_directory() . '/inc/jetpack.php';
 }
+
+add_filter( 'jetpack_implode_frontend_css', '__return_false', 99 );
