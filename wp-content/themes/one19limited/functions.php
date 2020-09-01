17d1bd69dbd6ff888603fbc545f93d8a6b88303b
Christophe Craig <christophe.craig@viacesi.fr>
2020-09-02 00:05:07 +1200

    restored wp_block css

--- wp-content/themes/one19limited/functions.php
+++ wp-content/themes/one19limited/functions.php
@@ -246,7 +246,7 @@ function dm_remove_wp_block_library_css()
 {
     wp_dequeue_style('wp-block-library');
 }
-add_action('wp_enqueue_scripts', 'dm_remove_wp_block_library_css');
+// add_action('wp_enqueue_scripts', 'dm_remove_wp_block_library_css');
 
 /**
  * Implement the Custom Header feature.
